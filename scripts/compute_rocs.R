59aa4a956e256d29bd67c96cf81b6e1ca283beae
Eric C. Anderson <eric.anderson@noaa.gov>
2025-01-13 15:43:10 -0700

    Modify ROC code to allow dropping likely FSPs
    I modified MUP to also report a logl for full-sibs.
    If that logl is > than it is for parental, I toss don't consider the individual for parentage.
    
    We now have another column `full_sib_treatment`.

--- scripts/compute_rocs.R
+++ scripts/compute_rocs.R
@@ -18,18 +18,26 @@ if(exists("snakemake")) {
   outrds <- snakemake@output$outrds
 } else {
   #inrds  <- "results/scenario-nonWF_simple/ps1-1200-ps2-1200-mr1-0.06-mr2-0.02/rep-0/ppn-0.5-verr-0.01-derr-0.004-vmiss-0.25-dmiss-0.25/mup_all_pairs.rds"
-  inrds <- "results/scenario-nonWF_simple/ps1-1200-ps2-1200-mr1-0.06-mr2-0.02/rep-0/ppn-0.5-verr-0.01-derr-0.004-vmiss-0.25-dmiss-0.25/hot_only_var_all_pairs.rds"
-  outrds <- "results/scenario-nonWF_simple/ps1-1200-ps2-1200-mr1-0.06-mr2-0.02/rep-0/ppn-0.5-verr-0.01-derr-0.004-vmiss-0.25-dmiss-0.25/mup_rocs.rds"
+  #inrds <- "results/scenario-nonWF_simple/ps1-1200-ps2-1200-mr1-0.06-mr2-0.02/rep-0/ppn-0.5-verr-0.01-derr-0.004-vmiss-0.25-dmiss-0.25/hot_only_var_all_pairs.rds"
+  inrds = "test-mup-logls.rds"
+  outrds = "test-compute-rocs.rds"
+  #outrds <- "results/scenario-nonWF_simple/ps1-1200-ps2-1200-mr1-0.06-mr2-0.02/rep-0/ppn-0.5-verr-0.01-derr-0.004-vmiss-0.25-dmiss-0.25/mup_rocs.rds"
 }
 
 
 trimmed_pairs <- read_rds(inrds)
+
+MUP_used <- FALSE
+if("logl_ratio" %in% names(trimmed_pairs)) {
+  MUP_used <- TRUE
+}
 
 # after filtering out different candidate parent sets, this will compute the ROC
 # this is set up so that it uses a metric that increases for pairs that are
 # more likely (or have fewer mendelian incompatibilities).  We also
 # set up what kind of inference procedure we used.
 make_roc_mup <- function(X) {
+
   if("hot_fract" %in% names(X)) {
     X2 <- X %>%
       mutate(
@@ -90,15 +98,33 @@ make_roc_mup <- function(X) {
 
 
 # now we can do the two groups we want to
-ret <- list(
+first_ret <- list(
   exclude_same_cohort = trimmed_pairs %>%
     filter(par_time > 0) %>%
     make_roc_mup(),
   include_same_cohort = trimmed_pairs %>%
     make_roc_mup()
 ) %>%
-  bind_rows(.id = "cohort_inclusion")
+  bind_rows(.id = "cohort_inclusion") %>%
+  mutate(full_sib_treatment = "none", .after = cohort_inclusion)
 
+# and we can also use the Full sib logl to discard likely full-sibs
+# as parents, first.
+next_ret <- NULL
 
+if(MUP_used) {
+  next_ret <- list(
+    exclude_same_cohort = trimmed_pairs %>%
+      filter(par_time > 0, probKidParental > probKidFullSib) %>%
+      make_roc_mup(),
+    include_same_cohort = trimmed_pairs %>%
+      filter(probKidParental > probKidFullSib) %>%
+      make_roc_mup()
+  ) %>%
+    bind_rows(.id = "cohort_inclusion") %>%
+    mutate(full_sib_treatment = "discarded_likely_FS", .after = cohort_inclusion)
+}
+
+ret <- bind_rows(first_ret, next_ret)
 
 write_rds(ret, file = outrds)
